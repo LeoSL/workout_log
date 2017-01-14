3da445152a38be1881c81ba34c6f9f4324613ae3
LeoSL <leosilvalima@gmail.com>
2017-01-14 18:20:15 -0200

    add workouts schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20151123233243) do
+
   create_table "workouts", force: :cascade do |t|
     t.datetime "date"
     t.string   "workout"
@@ -20,4 +21,5 @@ ActiveRecord::Schema.define(version: 20151123233243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
