ef554df7feb27dceaeba82381ca2f6cbf70a9b92
LeoSL <leosilvalima@gmail.com>
2017-01-14 20:40:25 -0200

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :workouts
   root 'workouts#index'
+
+  devise_for :user, path: '', path_names: { sign_in: "login", sign_out: "logout" }
+  devise_scope :user do
+    get "/login", to: "devise/sessions#new", :as => :login
+    get "/logout", to: "devise/sessions#destroy", :as => :logout
+  end
 end
